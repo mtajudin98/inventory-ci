005a1899b16389ddb2427bdbef0561acd6cd68c2
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2019-12-13 20:23:54 +0700

    Ubah Layout Admin

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -95,7 +95,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <a href="index3.html" class="brand-link">
       <img src="<?php echo base_url();?>assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin Home</span>
+      <span class="brand-text font-weight-light">IManagement</span>
     </a>
 
     <!-- Sidebar -->
@@ -106,7 +106,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="<?php echo base_url();?>assets/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Mimin D</a>
         </div>
       </div>
 
