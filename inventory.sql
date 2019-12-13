43ea6a03ace2e87cb36895143dfb608b9f9e64f4
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2019-12-13 19:44:27 +0700

    ubah db

--- inventory.sql
+++ inventory.sql
@@ -1,57 +1,96 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2019 at 01:43 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
- Source Server         : mar
- Source Server Type    : MariaDB
- Source Server Version : 100408
- Source Host           : localhost:3306
- Source Schema         : inventory
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MariaDB
- Target Server Version : 100408
- File Encoding         : 65001
 
- Date: 16/11/2019 20:25:17
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `inventory`
+--
 
--- ----------------------------
--- Table structure for t_jenis
--- ----------------------------
-DROP TABLE IF EXISTS `t_jenis`;
-CREATE TABLE `t_jenis`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_jenis` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `user_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for t_produk
--- ----------------------------
-DROP TABLE IF EXISTS `t_produk`;
-CREATE TABLE `t_produk`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kode_produk` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `jenis_id` int(11) NULL DEFAULT NULL,
-  `nama_produk` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `harga` decimal(10, 0) NULL DEFAULT NULL,
-  `user_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`, `kode_produk`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+--
+-- Table structure for table `t_produk`
+--
 
--- ----------------------------
--- Table structure for t_user
--- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `access` int(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `t_produk` (
+  `id` int(11) NOT NULL,
+  `kode_produk` varchar(25) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `jenis_produk` enum('makanan','minuman','','') NOT NULL,
+  `harga_produk` int(11) NOT NULL,
+  `stock` int(3) NOT NULL,
+  `user_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Dumping data for table `t_produk`
+--
+
+INSERT INTO `t_produk` (`id`, `kode_produk`, `nama_produk`, `jenis_produk`, `harga_produk`, `stock`, `user_id`) VALUES
+(1, 'B1', 'Jus Jeruk', 'minuman', 10000, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(150) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `access` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `t_produk`
+--
+ALTER TABLE `t_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `t_produk`
+--
+ALTER TABLE `t_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
