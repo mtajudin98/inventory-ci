67eeb1d2508cdb0bf193bfe9e6d2af596e4a6ced
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2019-12-29 17:42:38 +0700

    Finishing

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -47,15 +47,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <span>Account</span>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">Settings</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i>Change Password
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i>Change Profile
-          </a>
           <div class="dropdown-divider"></div>
           <a href="<?php echo base_url(); ?>/user/logout" class="dropdown-item">
             <i class="fas fa-users mr-2"></i>Logout
